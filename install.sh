adc0fdaf4bb32de748a7fe22daff2aa68cab3028
undergroundfanclub <ungder@protonmail.ch>
2021-01-21 06:40:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 echo "installing dependacies"
-sudo apt install -y pulseaudio xinit xorg mpv suckless-tools vim nm-tray nitrogen scrot
+sudo apt install -y pulseaudio xinit xorg mpv suckless-tools vim nm-tray nitrogen scrot firefox-esr
 echo "install bspwm"
-apt install -y bspwm
+apt install -y bspwm sxhkd
 mkdir ~/.config/bspwm
 mkdir ~/.config/sxhkd
 cp bsconf/bspwmrc~/.config/bspwm/bspwmrc
 chmod +x ~/.config/bspwm/bspwmrc 
 cp bsconf/sxhkdrc ~/.config/sxhkd/sxhkdrc
+sudo cp bspwm/contrib/freedesktop/bspwm.desktop /usr/share/xsessions
 
 echo "grabbing alacritty"
 wget https://github.com/alacritty/alacritty/releases/download/v0.4.2/Alacritty-v0.4.2-ubuntu_18_04_amd64.deb
 apt install -y ./Alacritty-v0.4.2-ubuntu_18_04_amd64.deb
 mkdir ~/.config/alacritty/
-cp alacritty.yml ~/.config/alacratty/alacritty.yml
-echo "alacrotty installed"
+cp alacritty.yml ~/.config/alacritty/alacritty.yml
+echo "alacritty installed"
 
 echo "installing vim"
 cp .vimrc ~/.vimrc
 echo "vim installed"
-
-echo "would you like (i)cecat/iceweasal or (f)irefox?"
-read browser
-#if [$browser = f];then
-#       apt install firefox
-#elif [$browser = i];then
-# 	apt install iceweasel
-#fi	
 
 echo "touching up"
 sudo update-alternatives --config editor
