78aae284a69543d284e5000e161a37d22c84993e
undergroundfanclub <ungder@protonmail.ch>
2021-03-08 15:51:01 -0500

    modified:   install.sh 	modified:   shellaliases

--- install.sh
+++ install.sh
@@ -12,6 +12,21 @@ else
 	exit
 fi
 
+updatebackports () {echo "test"}
+#{echo "deb http://deb.debian.org/debian stretch-backports main" | tee -a /etc/apt/sources.list
+#apt-get update}
+echo "would you like to use backports?[y/n]"
+read yon
+if ["$yon" = y]; then
+    updatebackports
+elif [ "$yon" = n ]; then
+    echo "your loss"
+else
+    echo "Not a valid answer."
+    sleep 2
+
+fi
+
 echo "installing dependacies"
 sudo apt install pulseaudio pavucontrol bspwm tint2 xinit conky libnotify4 fonts-firacode xorg
 lightdm vim feh scrot firefox-esr wget gcc make cmake cbatticon volumeicon-alsa
@@ -27,15 +42,28 @@ cp bsconf/sxhkdrc ~/.config/sxhkd/sxhkdrc
 echo "bspwm installed"
 
 echo "grabbing alacritty"
-wget https://github.com/alacritty/alacritty/releases/download/v0.4.2/Alacritty-v0.4.2-ubuntu_18_04_amd64.deb
-sudo apt install -y ./Alacritty-v0.4.2-ubuntu_18_04_amd64.deb
-mkdir ~/.config/alacritty/
-cp alacritty.yml ~/.config/alacritty/alacritty.yml
-echo "alacritty installed"
+#wget https://github.com/alacritty/alacritty/releases/download/v0.4.2/Alacritty-v0.4.2-ubuntu_18_04_amd64.deb
+#sudo apt install -y ./Alacritty-v0.4.2-ubuntu_18_04_amd64.deb
+#mkdir ~/.config/alacritty/
+#cp alacritty.yml ~/.config/alacritty/alacritty.yml
+#echo "alacritty installed"
 
 echo "installing vim"
 cp .vimrc ~/.vimrc
 echo "vim installed"
+
+yesvirt () { sudo apt install qemu virt-manager; }
+novirt () { echo "will not install qemu nor virt-manager"; }
+echo "Would you like to add qemu and virt-manager(frontend)? (y/n)"
+read yesorno
+if [ "$yesorno" = y ]; then
+    yesvirt
+elif [ "$yesorno" = n ]; then
+    novirt
+else
+    echo "Not a valid answer."
+    sleep 2
+fi
 
 echo "touching up"
 sudo update-alternatives --config editor
@@ -51,5 +79,4 @@ mkdir ~/.config/tint2
 cp tint2rc ~/.config/tint2/
 cp .conkyrc ~/
 source ~/.bashrc
-echo 'polly debian flavor has been installed!
-please restart'
+echo "polly debian flavor has been installed! \n please restart"
