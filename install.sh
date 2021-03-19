fd4c6b363c693c1de8c1adb669d9b4d22983c5a2
undergroundfanclub <ungder@protonmail.ch>
2021-03-18 22:30:03 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,18 +3,16 @@
 # Detect OS
 # $os_version variables aren't always in use, but are kept here for convenience
 if [[ -e /etc/debian_version ]]; then
-	os="debian"
-	os_version=$(grep -oE '[0-9]+' /etc/debian_version | head -1)
-	group_name="nogroup"
+        os="debian"
+        os_version=$(grep -oE '[0-9]+' /etc/debian_version | head -1)
+        group_name="nogroup"
 else
-	echo "This installer seems to be running on an unsupported distribution. \n
+        echo "This installer seems to be running on an unsupported distribution. \n
   this script is written for Debian 10 (Buster) main"
-	exit
+        exit
 fi
 
-updatebackports () {echo "test"}
-#{echo "deb http://deb.debian.org/debian stretch-backports main" | tee -a /etc/apt/sources.list
-#apt-get update}
+updatebackports () { echo "deb http://deb.debian.org/debian stretch-backports main" | tee -a /etc/apt/sources.list | apt-get update;}
 echo "would you like to use backports?[y/n]"
 read yon
 if ["$yon" = y]; then
@@ -30,7 +28,7 @@ fi
 echo "installing dependacies"
 sudo apt install pulseaudio pavucontrol bspwm tint2 xinit conky libnotify4 fonts-firacode xorg
 lightdm vim feh scrot firefox-esr wget gcc make cmake cbatticon volumeicon-alsa
-network-manager-gnome suckless-tools
+network-manager-gnome suckless-tools kitty
 mkdir ~/.config
 
 mkdir ~/.config/bspwm
@@ -41,12 +39,10 @@ chmod +x ~/.config/bspwm/bspwmrc
 cp bsconf/sxhkdrc ~/.config/sxhkd/sxhkdrc
 echo "bspwm installed"
 
-echo "grabbing alacritty"
-#wget https://github.com/alacritty/alacritty/releases/download/v0.4.2/Alacritty-v0.4.2-ubuntu_18_04_amd64.deb
-#sudo apt install -y ./Alacritty-v0.4.2-ubuntu_18_04_amd64.deb
-#mkdir ~/.config/alacritty/
-#cp alacritty.yml ~/.config/alacritty/alacritty.yml
-#echo "alacritty installed"
+echo "installing kitty conf"
+mkdir ~/.config/kitty/
+cp kitty.conf ~/.config/kitty/
+echo "kitty installed"
 
 echo "installing vim"
 cp .vimrc ~/.vimrc
@@ -79,4 +75,4 @@ mkdir ~/.config/tint2
 cp tint2rc ~/.config/tint2/
 cp .conkyrc ~/
 source ~/.bashrc
-echo "polly debian flavor has been installed! \n please restart"
+echo 'polly debian flavor has been installed!\nplease restart'
